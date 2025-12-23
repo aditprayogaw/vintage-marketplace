1c7b77fea7aa0d70347efc894879de80b3a92bf4
adityaprayogaw <adityaprayoga1263@gmail.com>
2025-12-23 10:27:46 +0800

    Update Protection

--- src/FireBase.js
+++ src/FireBase.js
@@ -1,22 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCW2fKMxh7clmfrVDhXIu4NacvCTCn0k7c",
-  authDomain: "vintage-app-1aeb6.firebaseapp.com",
-  projectId: "vintage-app-1aeb6",
-  storageBucket: "vintage-app-1aeb6.firebasestorage.app",
-  messagingSenderId: "563545849030",
-  appId: "1:563545849030:web:d16f9e6ae0992c3797ebc7"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
-
+  
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-// Initialize Services
-const auth = getAuth(app);      
-const db = getFirestore(app);  
-const storage = getStorage(app);
-
-export { auth, db, storage};
+export { db, auth };
